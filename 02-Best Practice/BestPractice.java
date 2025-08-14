4776faba5b4da438bad6c71854fbaaa5e1c3eb21
Sathish Sarma <sathishsarma.edu@gmail.com>
2025-08-14 17:14:48 +0800

    Create BestPractice.java

---
+++ 02-Best Practice/BestPractice.java
@@ -0,0 +1,30 @@
+/**********************************************************************
+
+Program Title : Best Practices of Programming
+FileName: "BestPractice.java"
+
+Programmer : Sathish Sarma
+Contact Details : sathish.sarma@newinti.edu.my
+Date : 14 August 2025
+
+**********************************************************************/
+
+public class BestPractice {
+    
+    // Best practices of programming
+    public static void main(String[] args)
+    {
+        System.out.println("Hi, I'm Programmer Sathish Sarma.\n");
+		System.out.println("A good application should have following best practices of programming");
+		System.out.println("1. Clear Structure");
+		System.out.println("2. Identifiers with meaningful names");
+		System.out.println("3. Comments for providing context and understanding");
+		System.out.println("4. Broken down the complex program into manageable components");
+		System.out.println("5. Exception handling");
+		System.out.println("6. Follow Coding Standards");
+		System.out.println("7. Readable and Extensible");
+		System.out.println("");
+		System.out.println("What other aspects that you can think of for the best practices in programming?");
+		
+    }
+}
